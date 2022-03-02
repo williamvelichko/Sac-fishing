01a50bebf8e0904ab84e5ac6164951e700479cd1
williamvelichko <williamvelichko2003@gmail.com>
2022-03-01 16:29:04 -0800

    commit

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -1,10 +1,30 @@
 import React from "react";
 import { connect } from "react-redux";
+import styled from "styled-components";
 
 function Checkout(props) {
   const { basket } = props;
 
-  return <div></div>;
+  return (
+    <div>
+      <div>
+        {basket.map((item) => {
+          {
+            console.log(item);
+          }
+          return (
+            <Item key={item.id}>
+              <img src={item.image} alt="image" />
+              <Des>
+                <h2>{item.name}</h2>
+                <h3>${item.price}</h3>
+              </Des>
+            </Item>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 const mapState = (state) => {
   return {
@@ -12,3 +32,14 @@ const mapState = (state) => {
   };
 };
 export default connect(mapState)(Checkout);
+
+const Item = styled.div`
+  display: flex;
+  width: 40%;
+  img {
+    width: 30%;
+  }
+`;
+const Des = styled.div`
+  padding: 15px;
+`;
